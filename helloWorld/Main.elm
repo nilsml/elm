2a5134c920a75cbb7edf9abdf743734a54654e0c
Nils Magne Lunde <nilsml@nilsland.com>
2015-12-19 22:53:53 +0100

    My first counter

--- helloWorld/Main.elm
+++ helloWorld/Main.elm
@@ -1,4 +1,40 @@
-import Html exposing (text)
+import StartApp.Simple exposing (start)
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+
+
+type alias Model = Int
+
+type Action = Increment | Decrement
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div []
+    [ button [ onClick address Decrement ] [ text "-" ]
+    , div [ countStyle ] [ text (toString model) ]
+    , button [ onClick address Increment ] [ text "+" ]
+    ]
+
+countStyle : Attribute
+countStyle =
+  style
+    [ ("font-size", "20px")
+    , ("font-family", "monospace")
+    , ("display", "inline-block")
+    , ("width", "50px")
+    , ("text-align", "center")
+    ]
 
 main =
-  text "Hello, World!"
+  start
+    { model = 0
+    , update = update
+    , view = view
+    }
