868f4ef4985d90603e1036ceb087a255d0f05bb5
Nils Magne Lunde <nilsml@nilsland.com>
2015-12-19 23:06:34 +0100

    Modularization

--- helloWorld/Main.elm
+++ helloWorld/Main.elm
@@ -1,36 +1,6 @@
 import StartApp.Simple exposing (start)
-import Html exposing (..)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Counter exposing (view, update)
 
-
-type alias Model = Int
-
-type Action = Increment | Decrement
-
-update : Action -> Model -> Model
-update action model =
-  case action of
-    Increment -> model + 1
-    Decrement -> model - 1
-
-view : Signal.Address Action -> Model -> Html
-view address model =
-  div []
-    [ button [ onClick address Decrement ] [ text "-" ]
-    , div [ countStyle ] [ text (toString model) ]
-    , button [ onClick address Increment ] [ text "+" ]
-    ]
-
-countStyle : Attribute
-countStyle =
-  style
-    [ ("font-size", "20px")
-    , ("font-family", "monospace")
-    , ("display", "inline-block")
-    , ("width", "50px")
-    , ("text-align", "center")
-    ]
 
 main =
   start
